b7734ca72acfa35e76fd6d828515f1dde6608772
Damokless <damokless@protonmail.com>
2022-02-08 00:00:31 +0100

    Update ping command

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,6 @@ module.exports = {
     .setName('ping')
     .setDescription('Replies with Pong!'),
   async execute(interaction) {
-    await interaction.reply('Pong! on est la');
+    await interaction.reply('Pong!');
   },
 };
