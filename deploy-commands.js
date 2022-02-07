0786f41b12465364047553dffd1ca0d27e00c1fb
Damokless <damokless@protonmail.com>
2022-02-07 23:34:02 +0100

    adding dev and prod command deploy

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,6 +13,12 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
 
-rest.put(Routes.applicationCommands(process.env.BOT_CLIENT_ID), { body: commands })
+// Prod command
+/* rest.put(Routes.applicationCommands(process.env.BOT_CLIENT_ID), { body: commands })
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error); */
+
+// Dev command
+rest.put(Routes.applicationGuildCommands(process.env.BOT_CLIENT_ID, process.env.BOT_SERVER_ID), { body: commands })
   .then(() => console.log('Successfully registered application commands.'))
   .catch(console.error);
