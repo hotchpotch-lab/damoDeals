5f1000dfb39c3c58cc15c59aa01f9d5cafb1de9f
Damokless <damokless@protonmail.com>
2022-02-22 00:54:27 +0100

    Update search command

--- commands/fetchApi.js
+++ commands/fetchApi.js
@@ -3,16 +3,12 @@ const fetch = require('node-fetch');
 const { MessageEmbed } = require('discord.js');
 
 const todayDate = new Date();
-const date = (`0${todayDate.getDate()}`).slice(-2);
-const month = (`0${todayDate.getMonth() + 1}`).slice(-2);
-const year = todayDate.getFullYear();
-const hours = todayDate.getHours();
-const minutes = todayDate.getMinutes();
-const seconds = todayDate.getSeconds();
 let lastSyncDate = '2022-12-21 23:30:00';
+let dealsCount = 0;
 
 
 function sendData(data, interaction) {
+  dealsCount += 1;
   const givewayEmbed = new MessageEmbed()
     .setColor('#280137')
     .setTitle(data.title)
@@ -33,10 +29,17 @@ module.exports = {
 
     await interaction.reply('I\'m looking for new deals');
 
-    for (let i = 0; i < Object.keys(data).length; i++) {
-      data[i].published_date >= lastSyncDate ? sendData(data[i], interaction) : console.log('deal already sync');
+    // eslint-disable-next-line no-restricted-syntax
+    for (const deal of data) {
+      if (deal.published_date >= lastSyncDate) {
+        sendData(deal, interaction);
+      }
     }
-    lastSyncDate = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
-    console.log(lastSyncDate);
+
+    if (dealsCount === 0) {
+      interaction.channel.send(`No deals for the moment last search ${lastSyncDate}`);
+    }
+    lastSyncDate = todayDate.toISOString().replace('T', ' ').slice(0, -5);
+    console.log(`last search date : ${lastSyncDate}`);
   },
 };
