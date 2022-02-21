d2457a0553374eb248cdd848ca510706d61e8457
Damokless <damokless@protonmail.com>
2022-02-22 00:05:57 +0100

    Update search command

--- commands/fetchApi.js
+++ commands/fetchApi.js
@@ -2,25 +2,41 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const fetch = require('node-fetch');
 const { MessageEmbed } = require('discord.js');
 
+const todayDate = new Date();
+const date = (`0${todayDate.getDate()}`).slice(-2);
+const month = (`0${todayDate.getMonth() + 1}`).slice(-2);
+const year = todayDate.getFullYear();
+const hours = todayDate.getHours();
+const minutes = todayDate.getMinutes();
+const seconds = todayDate.getSeconds();
+let lastSyncDate = '2022-12-21 23:30:00';
+
+
+function sendData(data, interaction) {
+  const givewayEmbed = new MessageEmbed()
+    .setColor('#280137')
+    .setTitle(data.title)
+    .setAuthor({ name: 'damoDeals', url: 'https://github.com/Damokless' })
+    .setDescription(data.description)
+    .setImage(data.image)
+    .setFooter({ text: 'Made by Damokles' });
+  interaction.channel.send({ embeds: [givewayEmbed] });
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('sync')
-    .setDescription('sync deals api'),
+    .setName('search')
+    .setDescription('search for new deals'),
   async execute(interaction) {
     const response = await fetch('https://www.gamerpower.com/api/giveaways');
     const data = await response.json();
-    console.log(data[0]);
+
     await interaction.reply('I\'m looking for new deals');
-    for (let i = 0; i < 2; i++) {
-      const givewayEmbed = new MessageEmbed()
-        .setColor('#280137')
-        .setTitle(data[i].title)
-        .setAuthor({ name: 'damoDeals', url: 'https://github.com/Damokless' })
-        .setDescription(data[i].description)
-        .setImage(data[i].image)
-        .setFooter({ text: 'Made by Damokles' });
-      interaction.channel.send({ embeds: [givewayEmbed] });
+
+    for (let i = 0; i < Object.keys(data).length; i++) {
+      data[i].published_date >= lastSyncDate ? sendData(data[i], interaction) : console.log('deal already sync');
     }
+    lastSyncDate = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
+    console.log(lastSyncDate);
   },
 };
