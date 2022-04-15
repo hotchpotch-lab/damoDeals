768873b08db07b6df3e613f0e5def1d3d47b2275
Damokless <damokless@protonmail.com>
2022-04-15 17:07:48 +0200

    Update embed message

--- commands/fetchApi.js
+++ commands/fetchApi.js
@@ -12,7 +12,7 @@ function sendData(data, interaction) {
   const givewayEmbed = new MessageEmbed()
     .setColor('#280137')
     .setTitle(data.title)
-    .setUrl(data.open_giveaway)
+    .setURL(data.open_giveaway)
     .addFields(
       { name: 'Type', value: data.type },
       { name: 'Platforms', value: data.platforms, inline: true },
